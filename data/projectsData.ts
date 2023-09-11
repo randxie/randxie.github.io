3cbc48bff44710ee9ebe74699dcefbdc640428aa
randxie <randxiexyy29@gmail.com>
2023-09-10 19:22:39 -0700

    switch to use next.js

---
+++ data/projectsData.ts
@@ -0,0 +1,28 @@
+const projectsData = [
+  {
+    title: 'Open Source',
+    description: `In my spare time, I make contribution to open source projects.`,
+    imgSrc: '/static/images/projects/github_logo.png',
+    href: 'https://github.com/randxie',
+  },
+  {
+    title: 'DelTron Intelligence',
+    description: `I worked with DelTron Intelligence Technology to design and build Deltron's cloud AIoT platform from scratch. The platform has been in production to support N machines sending o(10)*N QPS predictions back to cloud. I also helped with optimizing ML inference pipeline speed and reliability at edge and built an internal data labeling system.`,
+    imgSrc: '/static/images/projects/deltron.png',
+    href: 'https://www.deltron.tech/',
+  },
+  {
+    title: 'Kaggle Competition Master',
+    description: `I was actively participating in Kaggle competitions from Oct 2016 to Jan 2018, and retired with highest ranking: 185/82368 and tier "Kaggle Competition Master"`,
+    imgSrc: '/static/images/projects/kaggle.png',
+    href: 'https://www.kaggle.com/randxie',
+  },
+  {
+    title: 'Predictive Maintenance Toolbox',
+    description: `Predictive maintenance toolbox is a product I built with a team of smart colleagues when I was working at MathWorks. It offers algorithms, visualization & deployment infrastructures for engineers and scientists to build their fault diagnostics and remaining useful life estimation capabilities.`,
+    imgSrc: '/static/images/projects/matlab.jpg',
+    href: 'https://www.mathworks.com/products/predictive-maintenance.html',
+  },
+]
+
+export default projectsData
