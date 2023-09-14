22d214eb03f9bdc7c2649bea1722fad59ad33d66
randxie <randxiexyy29@gmail.com>
2023-09-13 18:12:27 -0700

    update

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,9 +11,9 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
-          </h1>
+          <h3 className="text-3xl font-extra-bold leading-9 tracking-tight text-gray-400 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Life is short, Show me the stories
+          </h3>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
